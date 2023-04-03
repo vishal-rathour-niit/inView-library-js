1cdbc1b3ed911b0291389a359acfd8e0224699de
vishal rathour <47712561+vishalrathourownset@users.noreply.github.com>
2023-04-03 11:35:05 +0530

    Add files via upload

---
+++ inView.js
@@ -0,0 +1,62 @@
+function inView(options){
+    // developed by vishal rathour
+    // name :- inView 
+    // version :- 0.0.1
+    const rootOffset = options.rootOffset || '0px';
+    const threshold = options.threshold || 0;
+    const multipleGroup = options.multipleGroup || false;
+    const hasView = options.hasView || null;
+    const elements = options.element || null;
+
+    const start = {
+            init:function(){
+                start.render()
+            },
+            render:function(){
+                console.log('render start')
+                start.caching();
+            },
+            caching:function(){
+                let observer = new IntersectionObserver(start.callback, {
+                    root:null,
+                    rootMargin:rootOffset,
+                    threshold:threshold
+                });
+                if(multipleGroup === false){
+                    observer.observe(elements);
+                }
+                else
+                {
+                    try{
+                        elements.forEach(function(el){
+                            observer.observe(el);
+                        });
+                    }
+                    catch(e){
+                        console.error('please use {multipleGroup} false if you want to render single item.')
+                    }
+                }
+            },
+            callback:function(entries, observer){
+               if(entries.length == 1){
+                  if(entries[0].isIntersecting){
+                     hasView && hasView.call(start,entries[0].target,entries[0])
+                  }
+               }
+               else
+               {
+                    entries.forEach((entry) => {
+                        if(entry.isIntersecting){
+                            hasView && hasView.call(start,entry.target,entry)
+                          }
+                    })
+               }
+                
+            }
+
+
+    }
+    return start;
+
+
+}
