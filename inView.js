f0f1a67631d574627a2d535d5abe344dfbb33495
vishal rathour <47712561+vishal-rathour-niit@users.noreply.github.com>
2023-04-03 16:25:50 +0530

    Update inView.js

--- inView.js
+++ inView.js
@@ -1,13 +1,14 @@
 function inView(options){
     // developed by vishal rathour
     // name :- inView 
-    // version :- 0.0.1
+    // version :- 0.0.2
     const rootOffset = options.rootOffset || '0px';
     const threshold = options.threshold || 0;
     const multipleGroup = options.multipleGroup || false;
     const hasView = options.hasView || null;
     const elements = options.element || null;
-
+    let extraOnCallBack = null;
+   
     const start = {
             init:function(){
                 start.render()
@@ -40,16 +41,24 @@ function inView(options){
             callback:function(entries, observer){
                if(entries.length == 1){
                   if(entries[0].isIntersecting){
-                     hasView && hasView.call(start,entries[0].target,entries[0])
+                     hasView && hasView.call(start,entries[0].target,entries[0]);
+                     extraOnCallBack !== null && extraOnCallBack.call(start,entries[0].target,entries[0])
                   }
                }
                else
                {
                     entries.forEach((entry) => {
                         if(entry.isIntersecting){
-                            hasView && hasView.call(start,entry.target,entry)
-                          }
+                            hasView && hasView.call(start,entry.target,entry);
+                            extraOnCallBack !== null && extraOnCallBack.call(start,entry.target,entry)
+                        }
                     })
+               }
+                
+            },
+            on:function(event,handler){
+               if(event == 'hasView'){
+                  extraOnCallBack = handler;
                }
                 
             }
